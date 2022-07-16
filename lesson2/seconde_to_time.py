e59e301f82f3eac4ab2bceef6a312cb6f0b52390
Yasin Norozzadeh <88095232+yasinnorozzadeh@users.noreply.github.com>
2022-07-16 13:38:11 +0430

    Update seconde_to_time.py

--- lesson2/seconde_to_time.py
+++ lesson2/seconde_to_time.py
@@ -1,11 +1,13 @@
-saniye = int(input(' input saniyeha '))
-saat = saniye // 3600
-daghigheh =(saniye % saat)// 60
-saniye = saniye - saat*3600 - daghigheh*60
-if saniye > 60:
-    daghigheh =daghigheh + int(saniye / 60)
-    saniye =saniye - int(saniye % 60)
-elif daghigheh > 60 :
-    saat = saat + int(daghigheh / 60)
-    daghigheh = daghigheh - int(daghigheh % 60)
-print(int(saat) ,":",int(daghigheh),":",int(saniye))
+seconds = int(input("enter scondes for convert to hours:\n"))
+
+if seconds == 0:
+    print("error")
+else:
+    hours = int(seconds // 3600)
+    seconds -= hours * 3600
+    minutes = int(seconds // 60)
+    seconds -= minutes * 60
+    if hours >= 24:
+        days = int(hours // 24)
+        hours -= days * 24
+    print("time:", days, ":", hours, ":", int(minutes), ":", int(seconds))
