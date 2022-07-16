b341ca25947ffe9939c0f50a7c4631d1d872c99d
Yasin Norozzadeh <88095232+yasinnorozzadeh@users.noreply.github.com>
2022-07-16 14:08:18 +0430

    Update hangman.py

--- lesson3/hangman.py
+++ lesson3/hangman.py
@@ -1,20 +1,20 @@
 import random
 true_ch = []
 print("youre score : 10")
-joon = 10
+score = 10
 words = ["Book" , "Tree" , "Python" , "Bag" , "Umbrella" , "Dog" , "Clock" , "Engineer" , "Toothpast" , "Shirmoz"]
 word = random.choice(words)
 word = word.lower()
 print ("_ " * len(word) , ":")
 test = -1
-while not joon <= 0:
+while not score <= 0:
     user_charater = input("input youre character : ").lower()
     if user_charater in word :
         true_ch.append(user_charater)
         print("youre input true character\n")
     else:
-        joon -= 1
-        print("no |" , "youre score: " , joon)
+        score -= 1
+        print("no |" , "youre score: " , score)
     for i in range(len(word)):
         if word[i] in true_ch:
             print(word[i],end="") 
