14a7e77f46f1e2c3d855297e403964d673ef1752
Yasin Norozzadeh <88095232+yasinnorozzadeh@users.noreply.github.com>
2022-07-16 13:34:07 +0430

    Update average.py

--- lesson2/average.py
+++ lesson2/average.py
@@ -4,9 +4,13 @@ sum = 0
 for i in range(student_number):
     sum1 = float(input("input student score : "))
     student_list.append(sum1)
-    sum = sum + sum1
+
+for i in range(len(student_list)):
+    sum += student_list[i]
+
+average = sum / student_number
+
 student_list.sort()
-average = sum1 / student_number
 max = student_list[student_number-1]
 min = student_list[0]
-print("max average:" , max , "|" , "min average:" , min , "|" , "all average" ,  average)
+print("max score:" , max , "|" , "min score:" , min , "|" , "all average" ,  average)
