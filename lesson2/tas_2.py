15672cf3d67ee29b40707460a93501d9d6f1c72e
Yasin Norozzadeh <88095232+yasinnorozzadeh@users.noreply.github.com>
2022-07-16 13:42:44 +0430

    Update tas_2.py

--- lesson2/tas_2.py
+++ lesson2/tas_2.py
@@ -3,4 +3,4 @@ computer_tas = random.randint(1 , 6)
 print(computer_tas)
 while computer_tas == 6:
     computer_tas = random.randint(1 , 6)
-    print("jayze : " , computer_tas)
+    print("bonus : " , computer_tas)
