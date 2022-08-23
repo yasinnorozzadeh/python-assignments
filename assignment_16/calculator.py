c2c54a7f7d16280f8deb5965459727e2c08c10f7
Yasin Norozzadeh <88095232+yasinnorozzadeh@users.noreply.github.com>
2022-08-23 21:33:43 +0430

    Update calculator.py

--- assignment_16/calculator.py
+++ assignment_16/calculator.py
@@ -1,4 +1,5 @@
 import math
+from functools import partial
 from PySide6.QtWidgets import *
 from PySide6.QtUiTools import *
 from PySide6.QtCore import *
@@ -14,16 +15,18 @@ class HelloWorld(QMainWindow):
         self.ui.btn_sub.clicked.connect(self.sub)
         self.ui.btn_mul.clicked.connect(self.mul)
         self.ui.btn_div.clicked.connect(self.div)
-        self.ui.btn_num_1.clicked.connect(self.f_n_1)
-        self.ui.btn_num_2.clicked.connect(self.f_n_2)
-        self.ui.btn_num_3.clicked.connect(self.f_n_3)
-        self.ui.btn_num_4.clicked.connect(self.f_n_4)
-        self.ui.btn_num_5.clicked.connect(self.f_n_5)
-        self.ui.btn_num_6.clicked.connect(self.f_n_6)
-        self.ui.btn_num_7.clicked.connect(self.f_n_7)
-        self.ui.btn_num_8.clicked.connect(self.f_n_8)
-        self.ui.btn_num_9.clicked.connect(self.f_n_9)
-        self.ui.btn_num_0.clicked.connect(self.f_n_0)
+
+        self.ui.btn_num_1.clicked.connect(partial(self.numbers, 1))
+        self.ui.btn_num_2.clicked.connect(partial(self.numbers, 2))
+        self.ui.btn_num_3.clicked.connect(partial(self.numbers, 3))
+        self.ui.btn_num_4.clicked.connect(partial(self.numbers, 4))
+        self.ui.btn_num_5.clicked.connect(partial(self.numbers, 5))
+        self.ui.btn_num_6.clicked.connect(partial(self.numbers, 6))
+        self.ui.btn_num_7.clicked.connect(partial(self.numbers, 7))
+        self.ui.btn_num_8.clicked.connect(partial(self.numbers, 8))
+        self.ui.btn_num_9.clicked.connect(partial(self.numbers, 9))
+        self.ui.btn_num_0.clicked.connect(partial(self.numbers, 0))
+
         self.ui.btn_tan.clicked.connect(self.tan_n)
         self.ui.btn_cot.clicked.connect(self.cot_n)
         self.ui.btn_sin.clicked.connect(self.sin_n)
@@ -35,26 +38,9 @@ class HelloWorld(QMainWindow):
         self.ui.btn_dot.clicked.connect(self.dot)
         self.ui.btn_equal.clicked.connect(self.equal)
 
-    def f_n_1(self):
-        self.ui.text_box.setText(self.ui.text_box.text() + "1")
-    def f_n_2(self):
-        self.ui.text_box.setText(self.ui.text_box.text() + "2")
-    def f_n_3(self):
-        self.ui.text_box.setText(self.ui.text_box.text() + "3")
-    def f_n_4(self):
-        self.ui.text_box.setText(self.ui.text_box.text() + "4")
-    def f_n_5(self):
-        self.ui.text_box.setText(self.ui.text_box.text() + "5")
-    def f_n_6(self):
-        self.ui.text_box.setText(self.ui.text_box.text() + "6")
-    def f_n_7(self):
-        self.ui.text_box.setText(self.ui.text_box.text() + "7")
-    def f_n_8(self):
-        self.ui.text_box.setText(self.ui.text_box.text() + "8")
-    def f_n_9(self):
-        self.ui.text_box.setText(self.ui.text_box.text() + "9")
-    def f_n_0(self):
-        self.ui.text_box.setText(self.ui.text_box.text() + "0")
+    def numbers(self, x):
+        self.ui.text_box.setText(self.ui.text_box.text() + str(x))
+
     def dot(self):
         if "." not in self.ui.text_box.text():
             self.ui.text_box.setText(self.ui.text_box.text() + ".")
