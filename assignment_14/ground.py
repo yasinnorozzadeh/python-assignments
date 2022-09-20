34d6799981ba5c2c8c991a2ee46bea391e3d0942
Yasin Norozzadeh <88095232+yasinnorozzadeh@users.noreply.github.com>
2022-09-20 14:37:49 +0430

    Update ground.py

--- assignment_14/ground.py
+++ assignment_14/ground.py
@@ -1,51 +1,63 @@
-import random
 import arcade
-
-class Enemy_Bee(arcade.AnimatedWalkingSprite):
-    def __init__(self,x,s):
-        super().__init__()
-        self.walk_left_textures = [arcade.load_texture("imgg/bee_w1.png"),
-                                    arcade.load_texture("imgg/bee_w2.png"),
-                                    arcade.load_texture("imgg/bee_w3.png"),
-                                    arcade.load_texture("imgg/bee_w4.png")]
-        self.width = 5
-        self.height = 5
-        self.center_x = x
-        self.center_y = random.randint(200, 300)
-        self.change_x -= (s+1) 
-
-class Enemy_Co1(arcade.Sprite):
-    def __init__(self,x,s):
-        super().__init__("imgg\c1.png")
+import random
+class Ground(arcade.Sprite):
+    def __init__(self,x,y):
+        super().__init__(":resources:images/tiles/grassHalf_mid.png")
         self.width = 80
-        self.height = 100
-        self.center_y = 90
+        self.height = 80
         self.center_x = x
-        self.change_x -= s
+        self.center_y = y 
 
-class Enemy_Co2(arcade.Sprite):
-    def __init__(self,x,s):
-        super().__init__("imgg\c2.png")
-        self.width = 40
+class Cloud(arcade.Sprite):
+    def __init__(self):
+        super().__init__("imgg\cloud 1.png")
+        self.width = 100
         self.height = 100
-        self.center_x = x
-        self.center_y = 90
-        self.change_x -= s
+        self.center_x = 800
+        self.center_y = random.randint(350, 650)
+        self.change_x -= 0.5
 
-class Enemy_Co3(arcade.Sprite):
-    def __init__(self,x,s):
-        super().__init__("imgg\c3.png")
-        self.width = 70
+class Cloud1(arcade.Sprite):
+    def __init__(self):
+        super().__init__("imgg\cloud 2.png")
+        self.width = 100
         self.height = 100
-        self.center_x = x
-        self.center_y = 90
-        self.change_x -= s
+        self.center_x = 800
+        self.center_y = random.randint(350, 650)
+        self.change_x -= 0.5
 
-class Enemy_Co4(arcade.Sprite):
-    def __init__(self,x,s):
-        super().__init__("imgg\c4.png")
-        self.width = 65
-        self.height = 40
-        self.center_x = x
-        self.center_y = 65
-        self.change_x -= s
+class Cloud2(arcade.Sprite):
+    def __init__(self):
+        super().__init__("imgg\cloud 3.png")
+        self.width = 100
+        self.height = 100
+        self.center_x = 800
+        self.center_y = random.randint(350, 650)
+        self.change_x -= 0.5
+
+class Cloud3(arcade.Sprite):
+    def __init__(self):
+        super().__init__("imgg\cloud 4.png")
+        self.width = 100
+        self.height = 100
+        self.center_x = 800
+        self.center_y = random.randint(350, 650)
+        self.change_x -= 0.5
+
+class Cloud4(arcade.Sprite):
+    def __init__(self):
+        super().__init__("imgg\cloud 5.png")
+        self.width = 100
+        self.height = 100
+        self.center_x = 800
+        self.center_y = random.randint(350, 650)
+        self.change_x -= 0.5
+
+class Cloud5(arcade.Sprite):
+    def __init__(self):
+        super().__init__("imgg\cloud 6.png")
+        self.width = 100
+        self.height = 100
+        self.center_x = 800
+        self.center_y = random.randint(350, 650)
+        self.change_x -= 0.5
